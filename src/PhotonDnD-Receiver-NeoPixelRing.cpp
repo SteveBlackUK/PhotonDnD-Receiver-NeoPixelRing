5cee94db9da2eae35108c5843f374c4ba123d9d2
SteveBlackUK <steven.black@gmail.com>
2020-03-26 14:21:29 +0000

    commit of starting point photon files

---
+++ src/PhotonDnD-Receiver-NeoPixelRing.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/steve_black_spare/Dropbox/PhotonDnD-Receiver-NeoPixelRing/src/PhotonDnD-Receiver-NeoPixelRing.ino"
+/*
+ * Project PhotonDnD-Receiver-NeoPixelRing
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/Users/steve_black_spare/Dropbox/PhotonDnD-Receiver-NeoPixelRing/src/PhotonDnD-Receiver-NeoPixelRing.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
